131f0337ccdee0ce194323870e42f0e83bae8430
Francesco Vezzi <vezzi@nestor1.uppmax.uu.se>
2016-03-18 10:23:18 +0100

    when you realise how useful code review is....

--- taca_ngi_pipeline/deliver/deliver_mosler.py
+++ taca_ngi_pipeline/deliver/deliver_mosler.py
@@ -12,20 +12,10 @@ import time
 
 from deliver import *
 
-class MoslerDeliverer(Deliverer):
-
-    def __init__(self, projectid=None, sampleid=None, **kwargs):
-        super(MoslerDeliverer, self).__init__(
-            projectid,
-            sampleid,
-            **kwargs)
-        self.moslerdeliverypath = getattr(self, 'moslerdeliverypath', None)
-        self.moslersftpserver = getattr(self, 'moslersftpserver', None)
-        self.moslersftpserver_user = getattr(self, 'moslersftpserver_user', None)
-        self.moslersftpmaxfiles = getattr(self, 'moslersftpmaxfiles', None)
 
 
-class MoslerProjectDeliverer(MoslerDeliverer):
+
+class MoslerProjectDeliverer(ProjectDeliverer):
     """ This object takes care of delivering project samples to mosler.
         When delivering to Mosler the delivery can take place only at project level
     """
@@ -35,38 +25,6 @@ class MoslerProjectDeliverer(MoslerDeliverer):
             sampleid,
             **kwargs)
     
-    def all_samples_delivered(
-            self,
-            sampleentries=None):
-        """ Checks the delivery status of all project samples
-
-            :params sampleentries: a list of sample entry dicts to use instead
-                of fetching from database
-            :returns: True if all samples in this project has been successfully
-                delivered, False otherwise
-            THIS HAS BEEN COPIED AND PASTED FROM deliver.py ProjectDeliverer class
-        """
-        sampleentries = sampleentries or db.project_sample_entries(db.dbcon(), self.projectid).get('samples', [])
-        return all([self.get_delivery_status(sentry) == 'DELIVERED' for sentry in sampleentries])
-    
-    def db_entry(self):
-        """ Fetch a database entry representing the instance's project
-            :returns: a json-formatted database entry
-            :raises taca_ngi_pipeline.utils.database.DatabaseError:
-                if an error occurred when communicating with the database
-            THIS HAS BEEN COPIED AND PASTED FROM deliver.py ProjectDeliverer class
-        """
-        return db.project_entry(db.dbcon(), self.projectid)
-    
-    def update_delivery_status(self, status="DELIVERED"):
-        """ Update the delivery_status field in the database to the supplied 
-            status for the project specified by this instance
-            :returns: the result from the underlying api call
-            :raises taca_ngi_pipeline.utils.database.DatabaseError:
-                if an error occurred when communicating with the database
-            THIS HAS BEEN COPIED AND PASTED FROM deliver.py ProjectDeliverer class
-        """
-        return db.update_project(db.dbcon(), self.projectid, delivery_status=status)
 
     def deliver_project(self):
         """ Deliver all samples in a project to mosler
@@ -146,7 +104,7 @@ class MoslerProjectDeliverer(MoslerDeliverer):
 
 
 
-class MoslerSampleDeliverer(MoslerDeliverer):
+class MoslerSampleDeliverer(SampleDeliverer):
     """
         A class for handling sample deliveries to Mosler
     """
@@ -162,74 +120,6 @@ class MoslerSampleDeliverer(MoslerDeliverer):
     def deliver_sample_thread(self, sampleentry=None, result=None, index=None):
         result[index] = self.deliver_sample(sampleentry)
         return None
-
-    def deliver_sample(self, sampleentry=None):
-        """ Deliver a sample to the destination specified by the config.
-            Will check if the sample has already been delivered and should not
-            be delivered again or if the sample is not yet ready to be delivered.
-
-            :params sampleentry: a database sample entry to use for delivery,
-                be very careful with caching the database entries though since
-                concurrent processes can update the database at any time
-            :returns: True if sample was successfully delivered or was previously
-                delivered, False if sample was not yet ready to be delivered
-            :raises taca_ngi_pipeline.utils.database.DatabaseError: if an entry corresponding to this
-                sample could not be found in the database
-            :raises DelivererReplaceError: if a previous delivery of this sample
-                has taken place but should be replaced
-            :raises DelivererError: if the delivery failed
-            THIS HAS BEEN COPIED AND PASTED FROM deliver.py SampletDeliverer class
-        """
-        # propagate raised errors upwards, they should trigger notification to operator
-        try:
-            logger.info("Delivering {} to {}".format(
-                str(self), self.expand_path(self.deliverypath)))
-            try:
-                if self.get_analysis_status(sampleentry) != 'ANALYZED' \
-                        and not self.force:
-                    logger.info("{} has not finished analysis and will not be delivered".format(str(self)))
-                    return False
-                if self.get_delivery_status(sampleentry) == 'DELIVERED' \
-                        and not self.force:
-                    logger.info("{} has already been delivered".format(str(self)))
-                    return True
-                elif self.get_delivery_status(sampleentry) == 'IN_PROGRESS' \
-                        and not self.force:
-                    logger.info("delivery of {} is already in progress".format(
-                        str(self)))
-                    return False
-                elif self.get_delivery_status(sampleentry) == 'FAILED':
-                    logger.info("retrying delivery of previously failed sample {}".format(str(self)))
-            except db.DatabaseError as e:
-                logger.error(
-                    "error '{}' occurred during delivery of {}".format(
-                        str(e), str(self)))
-                raise
-            # set the delivery status to in_progress which will also mean that any concurrent deliveries
-            # will leave this sample alone
-            self.update_delivery_status(status="IN_PROGRESS")
-            #Skipping report generation for MOSLER.... I have more urgen problems right now....
-            # stage the delivery
-            if not self.stage_delivery():
-                raise DelivererError("sample was not properly staged")
-            logger.info("{} successfully staged".format(str(self)))
-            if not self.stage_only:
-                # perform the delivery
-                if not self.do_delivery():
-                    raise DelivererError("sample was not properly delivered")
-                logger.info("{} successfully delivered".format(str(self)))
-                # set the delivery status in database
-                self.update_delivery_status()
-                # write a delivery acknowledgement to disk
-                #self.acknowledge_delivery() # not sure what this does for now I silent you
-            return True
-        except DelivererInterruptedError:
-            self.update_delivery_status(status="NOT DELIVERED")
-            raise
-        except Exception:
-            self.update_delivery_status(status="FAILED")
-            raise
-
 
 
     def do_delivery(self):
@@ -265,28 +155,5 @@ class MoslerSampleDeliverer(MoslerDeliverer):
         self.sftp_client.close()
         # return True, if something went wrong an exception is thrown before this
         return True
-
-
-
-    def db_entry(self):
-        """ Fetch a database entry representing the instance's project and sample
-            :returns: a json-formatted database entry
-            :raises taca_ngi_pipeline.utils.database.DatabaseError:
-                if an error occurred when communicating with the database
-            THIS HAS BEEN COPIED AND PASTED FROM deliver.py SampletDeliverer class
-        """
-        return db.sample_entry(db.dbcon(), self.projectid, self.sampleid)
-
-    def update_delivery_status(self, status="DELIVERED"):
-        """ Update the delivery_status field in the database to the supplied 
-            status for the project and sample specified by this instance
-            :returns: the result from the underlying api call
-            :raises taca_ngi_pipeline.utils.database.DatabaseError:
-                if an error occurred when communicating with the database
-            THIS HAS BEEN COPIED AND PASTED FROM deliver.py SampletDeliverer class
-        """
-        return db.update_sample(db.dbcon(), self.projectid, self.sampleid, delivery_status=status)
-
-
 
 
